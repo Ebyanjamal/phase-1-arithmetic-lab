2ac908c3648c584fdd9c26f175978fa671e2d7f9
Ebyan Jamal <Ebyanjamal6@gmail.com>
2023-01-01 20:47:47 -0700

    commit tree 99bbb749e7469ba54a9cefcaf5aea730e8a5c223

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
+let random = 7
+const num3 = 16;
+const num4 = 6;
+let mod = num3 % num4;
+ const max = Math.max( 2,3,6,20)
 
-// Write your code here
+
+
+
+
+
